2dd75492d8b582d6000b6285ef5250112faa59eb
ram <meerdara@hotmail.com>
2021-07-16 17:02:56 -0400

    set up engine structure, dummy game runs OK

---
+++ app/src/main/java/gl/engine/GameEngine.java
@@ -0,0 +1,83 @@
+package gl.engine;
+
+public class GameEngine implements Runnable {
+
+    private final Window window;
+    private final Timer timer;
+
+    private final IGameLogic gameLogic;
+    private static final int TARGET_UPS = 30;
+    private static final int TARGET_FPS = 75;
+
+    public GameEngine(String title, int width, int height, boolean vSync, IGameLogic gameLogic) {
+        window = new Window(title, width, height, vSync);
+        timer = new Timer();
+        this.gameLogic = gameLogic;
+    }
+
+    @Override
+    public void run() {
+        try {
+            init();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        gameLoop();
+    }
+
+    protected void init() throws Exception {
+        window.init();
+        timer.init();
+        gameLogic.init();
+    }
+
+    protected void gameLoop() {
+        float elapsedTime;
+        float accumulator = 0f;
+        float interval = 1f / TARGET_UPS;
+
+        boolean running = true;
+        while (running && !window.windowShouldClose()) {
+            elapsedTime = timer.getElapsedTime();
+            accumulator += elapsedTime;
+
+            input();
+
+            while (accumulator >= interval) {
+                update(interval);
+                accumulator -= interval;
+            }
+
+            render();
+
+            if (!window.isvSync()) {
+                sync();
+            }
+        }
+    }
+
+    private void sync() {
+        float loopSlot = 1f / TARGET_FPS;
+        double endTime = timer.getLastLoopTime() + loopSlot;
+        while (timer.getTime() < endTime) {
+            try {
+                Thread.sleep(1);
+            } catch (InterruptedException ie) {
+            }
+        }
+    }
+
+    protected void render() {
+        this.gameLogic.render(this.window);
+        this.window.update();
+    }
+
+    protected void update(float interval) {
+        this.gameLogic.update(interval);
+    }
+
+    protected void input() {
+        this.gameLogic.input(this.window);
+    }
+
+}
---
+++ app/src/main/java/gl/engine/IGameLogic.java
@@ -0,0 +1,12 @@
+package gl.engine;
+
+public interface IGameLogic {
+
+    void init() throws Exception;
+
+    void input(Window window);
+
+    void update(float interval);
+
+    void render(Window window);
+}
