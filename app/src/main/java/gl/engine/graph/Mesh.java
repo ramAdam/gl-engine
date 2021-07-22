526644611f31aee540fb5d23756495eb8b687fcc
ram <meerdara@hotmail.com>
2021-07-21 23:05:50 -0400

    implemented obj model loader

--- app/src/main/java/gl/engine/graph/Mesh.java
+++ app/src/main/java/gl/engine/graph/Mesh.java
@@ -5,6 +5,7 @@ import java.nio.IntBuffer;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.joml.Vector3f;
 import org.lwjgl.system.MemoryUtil;
 import static org.lwjgl.opengl.GL30.*;
 import static org.lwjgl.opengl.GL13.GL_TEXTURE0;
@@ -13,20 +14,24 @@ import static org.lwjgl.opengl.GL11.glBindTexture;
 
 public class Mesh {
 
+    private static final Vector3f DEFAULT_COLOUR = new Vector3f(1.0f, 1.0f, 1.0f);
+
     private final int vaoId;
     private final List<Integer> vboIdList;
     private final int vertexCount;
 
-    private final Texture texture;
+    private Texture texture;
+    private Vector3f colour;
 
-    public Mesh(float[] positions, float[] textCoords, int[] indices, Texture texture) {
+    public Mesh(float[] positions, float[] textCoords, float[] normals, int[] indices) {
 
         FloatBuffer posBuffer = null;
         FloatBuffer textCoordsBuffer = null;
         IntBuffer indicesBuffer = null;
+        FloatBuffer vecNormalsBuffer = null;
 
         try {
-            this.texture = texture;
+            this.colour = Mesh.DEFAULT_COLOUR;
             vertexCount = indices.length;
             vboIdList = new ArrayList<>();
 
@@ -53,6 +58,16 @@ public class Mesh {
             glEnableVertexAttribArray(1);
             glVertexAttribPointer(1, 2, GL_FLOAT, false, 0, 0);
 
+            // Vertex normals VBO
+            vboId = glGenBuffers();
+            vboIdList.add(vboId);
+            vecNormalsBuffer = MemoryUtil.memAllocFloat(normals.length);
+            vecNormalsBuffer.put(normals).flip();
+            glBindBuffer(GL_ARRAY_BUFFER, vboId);
+            glBufferData(GL_ARRAY_BUFFER, vecNormalsBuffer, GL_STATIC_DRAW);
+            glEnableVertexAttribArray(2);
+            glVertexAttribPointer(2, 3, GL_FLOAT, false, 0, 0);
+
             // Index VBO
             vboId = glGenBuffers();
             vboIdList.add(vboId);
@@ -73,7 +88,10 @@ public class Mesh {
             if(textCoordsBuffer != null){
                 MemoryUtil.memFree(textCoordsBuffer);
             }
-              if (indicesBuffer != null) {
+            if (vecNormalsBuffer != null) {
+                MemoryUtil.memFree(vecNormalsBuffer);
+            }
+            if (indicesBuffer != null) {
                 MemoryUtil.memFree(indicesBuffer);
             }
         }
@@ -81,8 +99,12 @@ public class Mesh {
 
     public void render() {
         // Activate firs texture bank
-        glActiveTexture(GL_TEXTURE0);
-        glBindTexture(GL_TEXTURE_2D, this.texture.getId());
+         if (texture != null) {
+            // Activate firs texture bank
+            glActiveTexture(GL_TEXTURE0);
+            // Bind the texture
+            glBindTexture(GL_TEXTURE_2D, texture.getId());
+        }
 
         // Draw the mesh
         glBindVertexArray(getVaoId());
@@ -91,6 +113,7 @@ public class Mesh {
 
         // Restore state
         glBindVertexArray(0);
+        glBindTexture(GL_TEXTURE_2D, 0);
 
     }
 
@@ -117,6 +140,25 @@ public class Mesh {
         // Delete the VAO
         glBindVertexArray(0);
         glDeleteVertexArrays(vaoId);
+    }
+    public void setColour(Vector3f colour){
+        this.colour = colour;
+    }
+
+    public Vector3f getColour() {
+        return this.colour;
+    }
+
+    public boolean isTextured() {
+        return this.texture != null;
+    }
+
+    public Texture getTexture() {
+        return this.texture;
+    }
+
+    public void setTexture(Texture texture) {
+        this.texture = texture;
     }
 
 }
--- app/src/main/java/gl/game/DummyGame.java
+++ app/src/main/java/gl/game/DummyGame.java
@@ -16,6 +16,7 @@ import gl.engine.MouseInput;
 import gl.engine.Window;
 import gl.engine.graph.Camera;
 import gl.engine.graph.Mesh;
+import gl.engine.graph.OBJLoader;
 import gl.engine.graph.Texture;
 
 public class DummyGame implements IGameLogic {
@@ -30,7 +31,6 @@ public class DummyGame implements IGameLogic {
 
     private final Vector3f cameraInc;
 
-    private int scaleInc = 0;
 
     private Camera camera;
 
@@ -44,117 +44,16 @@ public class DummyGame implements IGameLogic {
     public void init(Window window) throws Exception {
         
         renderer.init(window);
-        float[] positions = new float[] { 
-               // V0
-            -0.5f, 0.5f, 0.5f,
-            // V1
-            -0.5f, -0.5f, 0.5f,
-            // V2
-            0.5f, -0.5f, 0.5f,
-            // V3
-            0.5f, 0.5f, 0.5f,
-            // V4
-            -0.5f, 0.5f, -0.5f,
-            // V5
-            0.5f, 0.5f, -0.5f,
-            // V6
-            -0.5f, -0.5f, -0.5f,
-            // V7
-            0.5f, -0.5f, -0.5f,
-            
-            // For text coords in top face
-            // V8: V4 repeated
-            -0.5f, 0.5f, -0.5f,
-            // V9: V5 repeated
-            0.5f, 0.5f, -0.5f,
-            // V10: V0 repeated
-            -0.5f, 0.5f, 0.5f,
-            // V11: V3 repeated
-            0.5f, 0.5f, 0.5f,
 
-            // For text coords in right face
-            // V12: V3 repeated
-            0.5f, 0.5f, 0.5f,
-            // V13: V2 repeated
-            0.5f, -0.5f, 0.5f,
-
-            // For text coords in left face
-            // V14: V0 repeated
-            -0.5f, 0.5f, 0.5f,
-            // V15: V1 repeated
-            -0.5f, -0.5f, 0.5f,
-
-            // For text coords in bottom face
-            // V16: V6 repeated
-            -0.5f, -0.5f, -0.5f,
-            // V17: V7 repeated
-            0.5f, -0.5f, -0.5f,
-            // V18: V1 repeated
-            -0.5f, -0.5f, 0.5f,
-            // V19: V2 repeated
-            0.5f, -0.5f, 0.5f,
-        };
-        float[] textCoords = new float[]{
-            0.0f, 0.0f,
-            0.0f, 0.5f,
-            0.5f, 0.5f,
-            0.5f, 0.0f,
-            
-            0.0f, 0.0f,
-            0.5f, 0.0f,
-            0.0f, 0.5f,
-            0.5f, 0.5f,
-            
-            // For text coords in top face
-            0.0f, 0.5f,
-            0.5f, 0.5f,
-            0.0f, 1.0f,
-            0.5f, 1.0f,
-
-            // For text coords in right face
-            0.0f, 0.0f,
-            0.0f, 0.5f,
-
-            // For text coords in left face
-            0.5f, 0.0f,
-            0.5f, 0.5f,
-
-            // For text coords in bottom face
-            0.5f, 0.0f,
-            1.0f, 0.0f,
-            0.5f, 0.5f,
-            1.0f, 0.5f,
-        };
-        int[] indices = new int[] { 
-           // Front face
-            0, 1, 3, 3, 1, 2,
-            // Top Face
-            8, 10, 11, 9, 8, 11,
-            // Right face
-            12, 13, 7, 5, 12, 7,
-            // Left face
-            14, 15, 6, 4, 14, 6,
-            // Bottom face
-            16, 18, 19, 17, 16, 19,
-            // Back face
-            4, 6, 7, 5, 4, 7,
-         };
+        mesh = OBJLoader.loadMesh("/models/cube.obj");
         Texture texture = new Texture("textures/grassblock.png");
-        mesh = new Mesh(positions, textCoords, indices, texture);
+        mesh.setTexture(texture);
 
         GameItem gameItem1 = new GameItem(mesh);
         gameItem1.setScale(0.5f);
         gameItem1.setPosition(0, 0, -2);
-        GameItem gameItem2 = new GameItem(mesh);
-        gameItem2.setScale(0.5f);
-        gameItem2.setPosition(0.5f, 0.5f, -2);
-        GameItem gameItem3 = new GameItem(mesh);
-        gameItem3.setScale(0.5f);
-        gameItem3.setPosition(0, 0, -2.5f);
-        GameItem gameItem4 = new GameItem(mesh);
-        gameItem4.setScale(0.5f);
-        gameItem4.setPosition(0.5f, 0, -2.5f);
-        gameItems = new GameItem[]{gameItem1, gameItem2, gameItem3, gameItem4};
+   
+        gameItems = new GameItem[]{gameItem1};
     }
 
     @Override
